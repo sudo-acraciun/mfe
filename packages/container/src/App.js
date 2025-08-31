5a976989a5e20ded81ac2dad809b3e2a2280c856
sudo-acraciun <alex.craciun10@gmail.com>
2025-08-31 11:03:51 +0200

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+import MarketingApp from './components/MarketingApp.js';
+
+export default () => {
+    return <div>
+        <h1>Hi there!</h1>
+        <hr />
+        <MarketingApp />
+    </div>;
+};
